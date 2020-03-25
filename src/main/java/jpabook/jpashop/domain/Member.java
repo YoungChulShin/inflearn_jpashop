0154367779673155c43fc3d1e4e9c0d8db25a335
YoungChulShin <go1323@gmail.com>
2020-03-25 23:06:44 +0900

    XtoOne 개선

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,7 +23,8 @@ public class Member {
 
     @Embedded
     private Address address;
-    
+
+    @JsonIgnore
     @OneToMany(mappedBy = "member") // Order Table에 있는 member 필드
     private List<Order> orders = new ArrayList<>();
 }
