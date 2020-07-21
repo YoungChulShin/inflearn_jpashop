b7ff2b6e590ba194db4f88231077c3673a856c5e
YoungChulShin <go1323@gmail.com>
2020-07-22 06:24:45 +0900

    QueryDSL 반영

--- 2nd/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ 2nd/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,8 +1,14 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.QMember;
+import jpabook.jpashop.domain.QOrder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -22,6 +28,46 @@ public class OrderRepository {
     }
 
 //    public List<Order> findAll(OrderSearch orderSearch) {
+//        return em.createQuery("select o from Order o join o.member m" +
+//                " where o.status = :status" +
+//                " and m.name like :name ", Order.class)
+//                .setParameter("status", orderSearch.getOrderStatus())
+//                .setParameter("name", orderSearch.getMemberName())
+//                .setMaxResults(1000)
+//                .getResultList();
+//    }
+
+//    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
 //
 //    }
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch), nameLike(orderSearch))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression statusEq(OrderSearch orderSearch) {
+        if (orderSearch.getOrderStatus() == null) {
+            return null;
+        }
+
+        return QOrder.order.status.eq(orderSearch.getOrderStatus());
+    }
+
+    private BooleanExpression nameLike(OrderSearch orderSearch) {
+        if (!StringUtils.hasText(orderSearch.getMemberName())) {
+            return null;
+        }
+
+        return QMember.member.name.like(orderSearch.getMemberName());
+    }
 }
