0a2f94412b22f08ac1f78c72b133241f8913b5e8
YoungChulShin <go1323@gmail.com>
2020-10-10 23:51:48 +0900

    add entity class

---
+++ 3rd/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
