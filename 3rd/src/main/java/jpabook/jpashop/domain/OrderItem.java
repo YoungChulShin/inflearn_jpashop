b5f75d5ce7dfe3da60f65df4adcb63e914691606
YoungChulShin <go1323@gmail.com>
2020-10-11 23:02:55 +0900

    주문 기능 추가

--- 3rd/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ 3rd/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,14 +1,17 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
+@Entity
 @Getter
 @Setter
-@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -27,4 +30,29 @@ public class OrderItem {
     private int orderPrice; // 주문 가격
 
     private int count;  // 주문 수량
+
+    //== 생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+
+        return orderItem;
+    }
+
+    //== 비즌니스 로직
+
+    /**
+     * 취소
+     */
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
