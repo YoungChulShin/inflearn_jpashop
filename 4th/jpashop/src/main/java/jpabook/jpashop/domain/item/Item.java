22e10ba1fa076ccac6e4e907b7fa6aacf46c535e
YoungChulShin <go1323@gmail.com>
2021-04-07 05:39:42 +0900

    상품 기능 개발

--- 4th/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ 4th/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.domain.item;
 import java.util.ArrayList;
 import java.util.List;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,4 +29,18 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+
+        this.stockQuantity = restStock;
+    }
 }
